fca79041b077adf6a34ec35eeac221343231152e
C250454 <c250454@ccg.ac.jp>
2025-05-31 21:48:01 +0900

    add Bai1

--- Bai1.java
+++ Bai1.java
@@ -1,5 +1,8 @@
 public class Bai1 {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println(c);
     }
 }
